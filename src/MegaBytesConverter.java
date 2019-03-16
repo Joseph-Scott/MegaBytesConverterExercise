f568c51a57927a9e308eac4ecf7799b624e4d6bf
Joseph Scott <jbs198585@gmail.com>
2019-03-16 14:42:39 -0400

    solution

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -37,10 +37,12 @@
 public class MegaBytesConverter {
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
-        if (x) {
-            System.out.println(XX KB = YY MB and ZZ KB);
-        } else if (kiloBytes < 0) {
+        if (kiloBytes < 0) {
             System.out.println("Invalid Value");
+        } else {
+            int MB = kiloBytes / 1024;
+            int remainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + MB + " MB and " + remainder + " KB");
         }
     }
 
